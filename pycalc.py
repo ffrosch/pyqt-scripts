5cca76f29f9428691a2979d242745c6216dbee05
Florian Frosch <sturmpuls@posteo.de>
2020-12-14 16:02:10 +0100

    feat: Add display

--- pycalc.py
+++ pycalc.py
@@ -1,6 +1,13 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication,
+                             QMainWindow,
+                             QWidget,
+                             QGridLayout,
+                             QLineEdit,
+                             QPushButton,
+                             QVBoxLayout)
 
 __version__ = '0.1'
 __author__ = 'Leonadis Pozo Ramos, modified by Florian Frosch'
@@ -14,10 +21,24 @@ class PyCalcUi(QMainWindow):
         # Set main window properties
         self.setWindowTitle('PyCalc')
         self.setFixedSize(235, 235)
-        # Set the central widget (required for a class that inherits from QMainWindow
+        # Set general layout and central widget
+        self.generalLayout = QVBoxLayout()
+        # The central widget is required for a class that inherits from QMainWindow
         self._centralWidget = QWidget(self)
         self.setCentralWidget(self._centralWidget)
-
+        self._centralWidget.setLayout(self.generalLayout)
+        # Create the display and the buttons
+        self._createDisplay()
+        
+    def _createDisplay(self):
+        '''Create the display for the calculator'''
+        self.display = QLineEdit()
+        self.display.setFixedHeight(35)
+        self.display.setAlignment(Qt.AlignRight)
+        self.display.setReadOnly(True)
+        # Add the display to the general Layout
+        self.generalLayout.addWidget(self.display)
+        
 
 def main():
     '''Execute the program'''
